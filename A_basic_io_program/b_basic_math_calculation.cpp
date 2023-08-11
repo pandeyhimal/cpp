675d0b812402fa650f32063ac831bd3a6a948668
himalpandey <himalpandey2002@gmail.com>
2023-08-11 16:35:42 +0545

    12a34b56c

---
+++ A_basic_io_program/b_basic_math_calculation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum,diff,pro,quo;
+    cout << " enter any two numbers:";
+    cin >> a >> b ;
+    sum = a+b;
+    diff = a-b;
+    pro = a*b;
+    quo = a/b;
+    cout << " The sum of " << a << " and " << b << " is " << sum << endl;
+    cout << " The differenc of " << a << " and " << b << " is " << diff << endl;
+    cout << " The product of " << a << " and " << b << " is " << pro << endl;
+    cout << " The quotient of " << a << " and " << b << " is " << quo ;
+    return 0;
+}
